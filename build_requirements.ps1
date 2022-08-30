ee1819aa11376a3ec215f3af981e1e27443c8194
Nicolas Schwab <nicolas.schwab@ceff.ch>
2022-08-30 20:41:32 +0200

    Check if pip-tools is installed

--- build_requirements.ps1
+++ build_requirements.ps1
@@ -1,21 +1,26 @@
 $venv = $PSScriptRoot + '\.venv\Scripts'
+$piptools = $PSScriptRoot + '\.venv\Lib\piptools\__ini__.py'
 $activate = $venv + '\activate.ps1'
 $deactivate = $venv + '\deactivate.bat'
 
 if (Test-Path -Path $venv) {
-    $common = $PSScriptRoot + '\requirements\common.in'
-    $dev = $PSScriptRoot + '\requirements\dev.in'
-    $prod = $PSScriptRoot + '\requirements\prod.in'
-    $requirements = $PSScriptRoot + '\requirements.in'
+    if (Test-Path -Path $venv) {
+        $common = $PSScriptRoot + '\requirements\common.in'
+        $dev = $PSScriptRoot + '\requirements\dev.in'
+        $prod = $PSScriptRoot + '\requirements\prod.in'
+        $requirements = $PSScriptRoot + '\requirements.in'
 
-    & $activate
+        & $activate
 
-    python -m piptools compile $($common)
-    python -m piptools compile $($dev)
-    python -m piptools compile $($prod)
-    python -m piptools compile $($requirements)
+        python -m piptools compile $($common)
+        python -m piptools compile $($dev)
+        python -m piptools compile $($prod)
+        python -m piptools compile $($requirements)
 
-    & $deactivate
+        & $deactivate
+    } else {
+        "piptool is missing. Please install it with " + $venv + 'pip.exe install -m pip-tools'
+    }
 } else {
     "Virtual environment doesn't exist."
 }
